b8465d285914a2dbba6c17e6bf193aa03c388b4b
i3oi3ka <i3oi3ka@ukr.net>
2025-09-04 00:03:41 +0300

    refactor: Update contacts API endpoints and methods for consistency in naming

--- src/api/contacts.py
+++ src/api/contacts.py
@@ -4,70 +4,57 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db import get_db
-from schemas.contacts import NoteModel, NoteUpdate, NoteStatusUpdate, NoteResponse
-from services.contacts import NoteService
+from src.schemas.contacts import ContactModel, ContactResponse
+from src.services.contacts import ContactService
 
-router = APIRouter(prefix="/notes", tags=["notes"])
+router = APIRouter(prefix="/contacts")
 
 
-@router.get("/", response_model=List[NoteResponse])
+@router.get("/", response_model=List[ContactResponse])
 async def read_notes(
     skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)
 ):
-    note_service = NoteService(db)
-    notes = await note_service.get_notes(skip, limit)
-    return notes
+    contact_service = ContactService(db)
+    contacts = await contact_service.get_contacts(skip, limit)
+    return contacts
 
 
-@router.get("/{note_id}", response_model=NoteResponse)
-async def read_note(note_id: int, db: AsyncSession = Depends(get_db)):
-    note_service = NoteService(db)
-    note = await note_service.get_note(note_id)
-    if note is None:
+@router.get("/{contact_id}", response_model=ContactResponse)
+async def read_contact(contact_id: int, db: AsyncSession = Depends(get_db)):
+    contact_service = ContactService(db)
+    contact = await contact_service.get_note(contact_id)
+    if contact is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Note not found"
         )
-    return note
+    return contact
 
 
-@router.post("/", response_model=NoteResponse, status_code=status.HTTP_201_CREATED)
-async def create_note(body: NoteModel, db: AsyncSession = Depends(get_db)):
-    note_service = NoteService(db)
-    return await note_service.create_note(body)
+@router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
+async def create_contact(body: ContactModel, db: AsyncSession = Depends(get_db)):
+    contact_service = ContactService(db)
+    return await contact_service.create_contact(body)
 
 
-@router.put("/{note_id}", response_model=NoteResponse)
-async def update_note(
-    body: NoteUpdate, note_id: int, db: AsyncSession = Depends(get_db)
+@router.put("/{contact_id}", response_model=ContactResponse)
+async def update_contact(
+    body: ContactModel, contact_id: int, db: AsyncSession = Depends(get_db)
 ):
-    note_service = NoteService(db)
-    note = await note_service.update_note(note_id, body)
+    contact_service = ContactService(db)
+    contact = await contact_service.update_note(contact_id, body)
     if note is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Note not found"
         )
-    return note
+    return contact
 
 
-@router.patch("/{note_id}", response_model=NoteResponse)
-async def update_status_note(
-    body: NoteStatusUpdate, note_id: int, db: AsyncSession = Depends(get_db)
-):
-    note_service = NoteService(db)
-    note = await note_service.update_status_note(note_id, body)
-    if note is None:
+@router.delete("/{contact_id}", response_model=ContactResponse)
+async def remove_contact(contact_id: int, db: AsyncSession = Depends(get_db)):
+    note_service = ContactService(db)
+    contact = await note_service.remove_contact(contact_id)
+    if contact is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Note not found"
         )
-    return note
-
-
-@router.delete("/{note_id}", response_model=NoteResponse)
-async def remove_note(note_id: int, db: AsyncSession = Depends(get_db)):
-    note_service = NoteService(db)
-    note = await note_service.remove_note(note_id)
-    if note is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Note not found"
-        )
-    return note
+    return contact
