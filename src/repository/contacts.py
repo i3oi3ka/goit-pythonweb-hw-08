b924621f4865e7a0faed771ca785c9adad1b0ec1
i3oi3ka <i3oi3ka@ukr.net>
2025-09-03 23:50:37 +0300

    fix: Rename methods for consistency in contact repository

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -12,52 +12,37 @@ class ContactRepository:
     def __init__(self, session: AsyncSession):
         self.db = session
 
-    async def get_notes(self, skip: int, limit: int) -> List[Note]:
-        stmt = select(Note).options(selectinload(Note.tags)).offset(skip).limit(limit)
+    async def get_contacts(self, skip: int, limit: int) -> List[Contact]:
+        stmt = select(Contact).offset(skip).limit(limit)
         notes = await self.db.execute(stmt)
         return notes.scalars().all()
 
-    async def get_note_by_id(self, note_id: int) -> Note | None:
-        stmt = select(Note).options(selectinload(Note.tags)).filter_by(id=note_id)
+    async def get_contact_by_id(self, contact_id: int) -> Contact | None:
+        stmt = select(Contact).filter_by(id=contact_id)
         note = await self.db.execute(stmt)
         return note.scalar_one_or_none()
 
-    async def create_note(self, body: NoteModel, tags: List[Tag]) -> Note:
-        note = Note(**body.model_dump(exclude={"tags"}, exclude_unset=True), tags=tags)
-        self.db.add(note)
+    async def create_contact(self, body: ContactModel) -> Contact:
+        contact = Contact(**body.model_dump(exclude_unset=True))
+        self.db.add(contact)
         await self.db.commit()
-        await self.db.refresh(note)
-        return await self.get_note_by_id(note.id)
+        await self.db.refresh(contact)
+        return await self.get_contact_by_id(contact.id)
 
-    async def remove_note(self, note_id: int) -> Note | None:
-        note = await self.get_note_by_id(note_id)
-        if note:
-            await self.db.delete(note)
+    async def remove_contact(self, contact_id: int) -> Contact | None:
+        contact = await self.get_contact_by_id(contact_id)
+        if contact:
+            await self.db.delete(contact)
             await self.db.commit()
-        return note
+        return contact
 
-    async def update_note(
-        self, note_id: int, body: NoteUpdate, tags: List[Tag]
-    ) -> Note | None:
-        note = await self.get_note_by_id(note_id)
-        if note:
-            for key, value in body.dict(exclude={"tags"}, exclude_unset=True).items():
-                setattr(note, key, value)
-
-            if tags is not None:
-                note.tags = tags
+    async def update_contact(self, note_id: int, body: ContactModel) -> Contact | None:
+        contact = await self.get_contact_by_id(note_id)
+        if contact:
+            for key, value in body.dict(exclude_unset=True).items():
+                setattr(contact, key, value)
 
             await self.db.commit()
-            await self.db.refresh(note)
+            await self.db.refresh(contact)
 
-        return note
-
-    async def update_status_note(
-        self, note_id: int, body: NoteStatusUpdate
-    ) -> Note | None:
-        note = await self.get_note_by_id(note_id)
-        if note:
-            note.done = body.done
-            await self.db.commit()
-            await self.db.refresh(note)
-        return note
+        return contact
